925a72c8869ea6638584436b3c80179474ee5483
jawher <jawher.moussa@gmail.com>
2012-08-19 14:29:32 +0200

    Updated the declarative archetype to: - use base-package property instead of routes - remove package names from the routes file (can be inferred by the default controller factory) - add an index page

--- archetypes/maven/vinna-declarative-archetype/src/main/resources/archetype-resources/src/main/java/controllers/HelloControler.java
+++ archetypes/maven/vinna-declarative-archetype/src/main/resources/archetype-resources/src/main/java/controllers/HelloControler.java
@@ -5,6 +5,10 @@ import vinna.outcome.StringOutcome;
 
 public class HelloControler {
 
+    public Outcome index() {
+        return new StringOutcome("Go to /hello/{your name} for a free hug !");
+    }
+
     public Outcome sayHello(String name, String ohai) {
         return new StringOutcome(String.format("%s %s !", (ohai == null ? "Ohai" : ohai), name));
     }
