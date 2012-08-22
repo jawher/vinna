421c8aedc59035f67ab4a1ea6ccf7c41f887cb65
jawher <jawher.moussa@gmail.com>
2012-08-22 23:42:12 +0200

    Always set encoding to utf-8 in vinna responses

--- vinna/src/main/java/vinna/response/VinnaResponseWrapper.java
+++ vinna/src/main/java/vinna/response/VinnaResponseWrapper.java
@@ -1,10 +1,13 @@
 package vinna.response;
 
+import vinna.exception.VuntimeException;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 
 public class VinnaResponseWrapper extends HttpServletResponseWrapper {
@@ -14,6 +17,7 @@ public class VinnaResponseWrapper extends HttpServletResponseWrapper {
     public VinnaResponseWrapper(HttpServletResponse httpServletResponse) {
         super(httpServletResponse);
         this.httpServletResponse = httpServletResponse;
+        this.httpServletResponse.setCharacterEncoding("utf-8");
     }
 
 }
