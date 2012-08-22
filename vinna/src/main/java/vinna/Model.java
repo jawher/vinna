14dc9e2c631343fcfa1802aec96ffebee0fa86c3
jawher <jawher.moussa@gmail.com>
2012-08-23 01:06:30 +0200

    Add a Model object to hold model data to be passed to views among other usages

---
+++ vinna/src/main/java/vinna/Model.java
@@ -0,0 +1,29 @@
+package vinna;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Model extends HashMap<String, Object> {
+    public Model add(String k, Object v) {
+        put(k, v);
+        return this;
+    }
+
+    public Model merge(Map<String, Object> m) {
+        putAll(m);
+        return this;
+    }
+
+    public Model addReqParams() {
+        Map<String, Collection<String>> params = VinnaContext.get().request.getParams();
+        for (Map.Entry<String, Collection<String>> entry : params.entrySet()) {
+            if(entry.getValue().size()==1) {
+                put(entry.getKey(), entry.getValue().iterator().next());
+            } else {
+                put(entry.getKey(), entry.getValue());
+            }
+        }
+        return this;
+    }
+}
