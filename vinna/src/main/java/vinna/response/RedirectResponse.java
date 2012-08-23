3655d480f2865508e12078fa6c7588d113f08e49
jawher <jawher.moussa@gmail.com>
2012-08-23 08:19:25 +0200

    Expose different kinds of redirects as an enum

--- vinna/src/main/java/vinna/response/RedirectResponse.java
+++ vinna/src/main/java/vinna/response/RedirectResponse.java
@@ -1,16 +1,27 @@
 package vinna.response;
 
-import javax.servlet.http.HttpServletResponse;
-
 public class RedirectResponse extends ResponseBuilder {
+    public enum Kind {
+        MOVED_PERMANENTLY(301),
+        FOUND(302),
+        SEE_OTHER(303),
+        NOT_MODIFIED(304),
+        TEMPORARY_REDIRECT(307),
+        PERMANENT_REDIRECT(308);
+
+        public final int status;
+
+        Kind(int status) {
+            this.status = status;
+        }
+    }
 
     public RedirectResponse(String location) {
-        this(location, HttpServletResponse.SC_FOUND);
+        this(location, Kind.FOUND);
     }
 
-    public RedirectResponse(String location, int status) {
-        super(status);
+    public RedirectResponse(String location, Kind kind) {
+        super(kind.status);
         location(location);
     }
-
 }
