780637662da4618be213d87255c580b1e09c3d35
Eric Bellemon <eric.bellemon@gmail.com>
2012-08-27 23:03:25 +0200

    Fix programmatic and declarative samples projects

--- samples/declarative-sample/src/main/java/vinna/samples/declarative/Controller.java
+++ samples/declarative-sample/src/main/java/vinna/samples/declarative/Controller.java
@@ -1,40 +1,40 @@
 package vinna.samples.declarative;
 
-import vinna.outcome.Outcome;
-import vinna.outcome.StringOutcome;
+import vinna.response.Response;
+import vinna.response.StringResponse;
 
 import java.util.Collection;
 
 public class Controller {
 
-	public StringOutcome index() {
-		return new StringOutcome("hello here");
-	}
+    public Response index() {
+        return new StringResponse("hello here");
+    }
 
-	public StringOutcome test(int id) {
-		return new StringOutcome("Your id is " + id);
-	}
+    public Response test(int id) {
+        return new StringResponse("Your id is " + id);
+    }
 
-	public StringOutcome test2(String inject, Integer id) {
-		return new StringOutcome("Your id is " + id + "\n" + inject);
-	}
+    public Response test2(String inject, Integer id) {
+        return new StringResponse("Your id is " + id + "\n" + inject);
+    }
 
-	public StringOutcome test2(String inject, boolean yo, Integer id) {
-		return new StringOutcome("Your id is " + id + "\n" + inject + "\n" + yo);
-	}
+    public Response test2(String inject, boolean yo, Integer id) {
+        return new StringResponse("Your id is " + id + "\n" + inject + "\n" + yo);
+    }
 
-    public Outcome writeHeaders(Collection<String> headers) {
+    public Response writeHeaders(Collection<String> headers) {
         if (headers == null || headers.size() == 0) {
-            return new StringOutcome("no header");
+            return new StringResponse("no header");
         }
         StringBuilder builder = new StringBuilder();
         for (String header : headers) {
             builder.append(header);
         }
-        return new StringOutcome(builder.toString());
+        return new StringResponse(builder.toString());
     }
 
-    public Outcome writeHeader(String header) {
-        return new StringOutcome(header);
+    public Response writeHeader(String header) {
+        return new StringResponse(header);
     }
 }
--- samples/programmatic-sample/src/main/java/vinna/samples/programmatic/Controller.java
+++ samples/programmatic-sample/src/main/java/vinna/samples/programmatic/Controller.java
@@ -1,72 +1,72 @@
 package vinna.samples.programmatic;
 
-import vinna.outcome.Outcome;
-import vinna.outcome.StringOutcome;
+import vinna.response.Response;
+import vinna.response.StringResponse;
 
 import java.util.Collection;
 import java.util.Map;
 
 public class Controller {
 
-    public StringOutcome index() {
-        return new StringOutcome("hello here");
+    public Response index() {
+        return new StringResponse("hello here");
     }
 
-    public StringOutcome test(int id) {
-        return new StringOutcome("Your id is " + id);
+    public Response test(int id) {
+        return new StringResponse("Your id is " + id);
     }
 
-    public StringOutcome test2(String inject, Integer id) {
-        return new StringOutcome("Your id is " + id + "\n" + inject);
+    public Response test2(String inject, Integer id) {
+        return new StringResponse("Your id is " + id + "\n" + inject);
     }
 
-    public StringOutcome test2(String inject, boolean yo, Integer id) {
-        return new StringOutcome("Your id is " + id + "\n" + inject + "\n" + yo);
+    public Response test2(String inject, boolean yo, Integer id) {
+        return new StringResponse("Your id is " + id + "\n" + inject + "\n" + yo);
     }
 
-    public Outcome writeHeaders(Collection<String> headers) {
+    public Response writeHeaders(Collection<String> headers) {
         if (headers == null || headers.size() == 0) {
-            return new StringOutcome("no header");
+            return new StringResponse("no header");
         }
         StringBuilder builder = new StringBuilder();
         for (String header : headers) {
             builder.append(header);
             builder.append("\n");
         }
-        return new StringOutcome(builder.toString());
+        return new StringResponse(builder.toString());
     }
 
-    public Outcome writeHeader(String header) {
-        return new StringOutcome(header);
+    public Response writeHeader(String header) {
+        return new StringResponse(header);
     }
 
-    public Outcome printHeaders(Map<String, Collection<String>> headers) {
+    public Response printHeaders(Map<String, Collection<String>> headers) {
         if (headers == null || headers.size() == 0) {
-            return new StringOutcome("no header");
+            return new StringResponse("no header");
         }
         StringBuilder builder = new StringBuilder();
         for (Map.Entry<String, Collection<String>> entry : headers.entrySet()) {
             builder.append(entry.getKey() + ":" + entry.getValue() + "\n");
         }
-        return new StringOutcome(builder.toString());
+        return new StringResponse(builder.toString());
     }
 
-    public Outcome params(String a, String b, Integer c) {
-        return new StringOutcome(a + ":" + b + ":" + c);
+    public Response params(String a, String b, Integer c) {
+        return new StringResponse(a + ":" + b + ":" + c);
     }
 
-    public Outcome paramCollection(Collection<String> params) {
+    public Response paramCollection(Collection<String> params) {
         if (params == null || params.size() == 0) {
-            return new StringOutcome("no param");
+            return new StringResponse("no param");
         }
         StringBuilder builder = new StringBuilder();
         for (String param : params) {
             builder.append(param + "\n");
         }
-        return new StringOutcome(builder.toString());
+        return new StringResponse(builder.toString());
     }
 
-    public Outcome post(String title, String description) {
-        return new StringOutcome(title + " : " + description);
+    public Response post(String title, String description) {
+        return new StringResponse(title + " : " + description);
     }
 }
