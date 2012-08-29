1fb943f1bea633b8534c4eb8725a4d228dcd44fd
jawher <jawher.moussa@gmail.com>
2012-08-29 08:26:30 +0200

    Vinna validation error keys prefix can now be configured using the conf files subsystem (or the vinna filter's init-params) using the key validation-error-prefix

--- vinna/src/main/java/vinna/Validation.java
+++ vinna/src/main/java/vinna/Validation.java
@@ -3,11 +3,16 @@ package vinna;
 public class Validation {
     private boolean hasErrors = false;
     private Model model = new Model();
+    private String errorPrefix;
+
+    public Validation() {
+        errorPrefix = (String) VinnaContext.get().vinna.getConfig().get("validation-error-prefix");
+    }
 
     public Validation required(String value, String name) {
         if (value == null || value.trim().isEmpty()) {
             hasErrors = true;
-            model.put("error-" + name, Messages.format("vinna.required", name));
+            model.put(errorPrefix + name, Messages.format("vinna.required", name));
         }
         return this;
     }
