d9269b88b4ee914ee3a3bb3fc2d396dfe49e4189
jawher <jawher.moussa@gmail.com>
2012-09-15 01:18:31 +0200

    Reworked the templating Context (reference resolver) to simplify the implementation and to optimise it.
    - Removed the extension mechanism (was based on a map besides the real model object). To add extra prams to a context, sublass it and override resolve.
    - Added an accessor cache, to avoid doing reflection on every access.
    - Fixed the parent delegation model: now there is a proper distinction between a found but null value (don't delegate to the parent) and a not found value (delegate to the parent)
    - Fixed a bug in literal variables handling

--- vinna/src/main/java/vinna/template/Context.java
+++ vinna/src/main/java/vinna/template/Context.java
@@ -2,39 +2,36 @@ package vinna.template;
 
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
+import java.io.Serializable;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
+    private static final Object NOT_FOUND = new Object();
     private final Context parent;
-    private final Map<String, Object> data = new HashMap<>();
+    private final Object data;
     private final Object helper;
 
     public Context(Context parent, Object root) {
         this.parent = parent;
-        this.data.put(".", root);
+        this.data = root;
         this.helper = parent == null ? null : parent.helper;
     }
 
     public Context(Context parent, Object root, Object helper) {
         this.parent = parent;
-        this.data.put(".", root);
+        this.data = root;
         this.helper = helper;
-    }
-
-    public void add(String key, Object value) {
-        data.put(key, value);
     }
 
     public Object resolve(String key) {
         if (".".equals(key)) {
-            return data.get(key);
-        } else if (key.matches("'.+'")) {
-            Object res = internalResolve(key.substring(1, key.length() - 2));
-            if (res == null) {
+            return data;
+        } else if (key.charAt(0) == '\'' && key.endsWith("'")) {
+            Object res = internalResolve(data, key.substring(1, key.length() - 2));
+            if (res == NOT_FOUND) {
                 if (parent != null) {
                     return parent.resolve(key);
                 } else {
@@ -45,79 +42,190 @@ public class Context {
             }
         } else {
             String[] parts = key.split("\\.");
-            Context context = this;
-            Object res = null;
+            Object base = data;
             for (String part : parts) {
-                res = context.internalResolve(part);
-                if (res == null) {
+                base = internalResolve(base, part);
+                if (base == NOT_FOUND) {
                     if (parent != null) {
                         return parent.resolve(key);
                     } else {
                         return null;
                     }
-                } else {
-                    context = new Context(context, res, helper);
                 }
             }
-            return res;
+            return base;
         }
     }
 
-    private Object internalResolve(String key) {
-        if (data.containsKey(key)) {
-            return data.get(key);
-        } else {
-            Object root = data.get(".");
-            if (root instanceof Map) {
-                Map<String, Object> m = (Map<String, Object>) root;
-                if (m.containsKey(key)) {
-                    return m.get(key);
-                }
-            } else {
-                try {
-                    Method getter = new PropertyDescriptor(key, root.getClass()).getReadMethod();
-                    if (getter != null) {
-                        return getter.invoke(root);
-                    }
-                } catch (IntrospectionException e) {
-                    // nop
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-                try {
-                    Method method = root.getClass().getMethod(key);
-                    method.setAccessible(true);
-                    return method.invoke(root);
-                } catch (IllegalAccessException | NoSuchMethodException e) {
-                    // nop
-                } catch (InvocationTargetException e) {
-                    throw new RuntimeException(e);
-                }
-                try {
-                    Field field = root.getClass().getField(key);
-                    return field.get(root);
-                } catch (NoSuchFieldException | IllegalAccessException e) {
-                    //nop
-                }
+    private Object internalResolve(Object data, String key) {
+        if (data == null) {
+            throw new NullPointerException("Trying to access the property " + key + " on a null object");
+        }
+        if (data instanceof Map) {
+            Map<String, Object> m = (Map<String, Object>) data;
+            if (m.containsKey(key)) {
+                return m.get(key);
+            }
+        } else if (data != null) {
+            final Accessor accessor = accessorFor(data, key, helper);
+            if (accessor != null) {
+                return accessor.get(data, helper);
+            }
+        }
 
-                if (helper != null) {
-                    try {
-                        Method[] methods = helper.getClass().getDeclaredMethods();
-                        for (Method method : methods) {
-                            if (key.equals(method.getName()) && method.getParameterTypes().length == 1) {
-                                if (method.getParameterTypes()[0].isAssignableFrom(root.getClass())) {
-                                    return method.invoke(helper, root);
-                                }
-                            }
+        return NOT_FOUND;
+    }
+
+    private static class Key implements Serializable {
+        public final String name;
+        public final Class<?> clazz;
+
+        private Key(String name, Class<?> clazz) {
+            this.name = name;
+            this.clazz = clazz;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Key key = (Key) o;
+
+            if (!clazz.equals(key.clazz)) return false;
+            if (!name.equals(key.name)) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name.hashCode();
+            result = 31 * result + clazz.hashCode();
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return clazz + "#" + name;
+        }
+    }
+
+    //FIXME: not thread safe
+    private static Map<Key, Accessor> accessorCache = new HashMap<>();
+
+    private Accessor accessorFor(Object root, String key, Object helper) {
+        Key k = new Key(key, root.getClass());
+        if (accessorCache.containsKey(k)) {
+            return accessorCache.get(k);
+        } else {
+            try {
+                Method getter = new PropertyDescriptor(key, root.getClass()).getReadMethod();
+                if (getter != null) {
+                    final Accessor.MethodAccessor accessor = new Accessor.MethodAccessor(getter);
+                    accessorCache.put(k, accessor);
+                    return accessor;
+                }
+            } catch (IntrospectionException e) {
+                // nop
+            }
+            try {
+                Method method = root.getClass().getMethod(key);
+                method.setAccessible(true);
+                final Accessor.MethodAccessor accessor = new Accessor.MethodAccessor(method);
+                accessorCache.put(k, accessor);
+                return accessor;
+            } catch (NoSuchMethodException e) {
+                // nop
+            }
+            try {
+                Field field = root.getClass().getField(key);
+                final Accessor.FieldAccessor accessor = new Accessor.FieldAccessor(field);
+                accessorCache.put(k, accessor);
+                return accessor;
+            } catch (NoSuchFieldException e) {
+                //nop
+            }
+
+            if (helper != null) {
+                Method[] methods = helper.getClass().getDeclaredMethods();
+                for (Method method : methods) {
+                    if (key.equals(method.getName()) && method.getParameterTypes().length == 1) {
+                        if (method.getParameterTypes()[0].isAssignableFrom(root.getClass())) {
+                            final Accessor.HelperAccessor accessor = new Accessor.HelperAccessor(method);
+                            accessorCache.put(k, accessor);
+                            return accessor;
                         }
-                    } catch (IllegalAccessException e) {
-                        // nop
-                    } catch (InvocationTargetException e) {
-                        throw new RuntimeException(e);
                     }
                 }
             }
+
+            accessorCache.put(k, Accessor.NoAccessor.INSTANCE);
+            return Accessor.NoAccessor.INSTANCE;
         }
-        return null;
+    }
+
+    private interface Accessor {
+        Object get(Object root, Object helper);
+
+        public static class MethodAccessor implements Accessor {
+            private final Method getter;
+
+            public MethodAccessor(Method getter) {
+                this.getter = getter;
+            }
+
+            @Override
+            public Object get(Object root, Object helper) {
+                try {
+                    return getter.invoke(root);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        public static class FieldAccessor implements Accessor {
+            private final Field getter;
+
+            public FieldAccessor(Field getter) {
+                this.getter = getter;
+            }
+
+            @Override
+            public Object get(Object root, Object helper) {
+                try {
+                    return getter.get(root);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        public static class HelperAccessor implements Accessor {
+            private final Method method;
+
+            public HelperAccessor(Method method) {
+                this.method = method;
+            }
+
+            @Override
+            public Object get(Object root, Object helper) {
+                try {
+                    return method.invoke(helper, root);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        public static class NoAccessor implements Accessor {
+            public static final NoAccessor INSTANCE = new NoAccessor();
+
+            @Override
+            public Object get(Object root, Object helper) {
+                return NOT_FOUND;
+            }
+        }
     }
 }
--- vinna/src/main/java/vinna/template/IterBlock.java
+++ vinna/src/main/java/vinna/template/IterBlock.java
@@ -13,16 +13,38 @@ public class IterBlock implements BlockHandler {
         Collection<Object> coll = (Collection<Object>) value;
         int i = 0;
         for (Object o : coll) {
-            Context subContext = new Context(context, o);
-            subContext.add("#", i);
-            subContext.add("##", i + 1);
-            subContext.add("#first", i == 0);
-            subContext.add("#last", i == coll.size() - 1);
-
+            Context subContext = new IterContext(context, o, i, i == coll.size() - 1);
             for (LiquidbarsNode child : block.getChildren()) {
                 defaultBlockHandler.render(child, subContext, defaultBlockHandler, out);
             }
             i++;
+        }
+    }
+
+    private static class IterContext extends Context {
+        private final int index;
+        private final boolean last;
+
+        public IterContext(Context parent, Object root, int index, boolean last) {
+            super(parent, root);
+            this.index = index;
+            this.last = last;
+        }
+
+        @Override
+        public Object resolve(String key) {
+            switch (key) {
+                case "#":
+                    return index;
+                case "##":
+                    return index + 1;
+                case "#first":
+                    return index == 0;
+                case "#last":
+                    return last;
+                default:
+                    return super.resolve(key);
+            }
         }
     }
 }
